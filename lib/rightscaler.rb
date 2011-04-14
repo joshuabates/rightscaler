4d6e6ad792e4bc183ffa755fc3eb5d2702f3ef28
Joshua Bates <joshuabates@gmail.com>
2011-04-14 11:57:14 -0700

    Revert "0.0.4 works with Rails 3.0.0."
    This reverts commit b56ff968dd146c041cff176f5c5501f6bd921a1f.
    
    Conflicts:
    
    	rightscaler.gemspec

--- lib/rightscaler.rb
+++ lib/rightscaler.rb
@@ -1,10 +1,10 @@
-require 'active_support'
-require 'active_resource'
+require 'activesupport'
+require 'activeresource'
 require "logger"
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 module Rightscaler
-  VERSION = "0.0.4" # updated to match ../VERSION.yml
+  VERSION = "0.1"
 end
 
 require 'rightscaler/resource'
@@ -16,26 +16,14 @@ require 'rightscaler/manage/deployment'
 require 'rightscaler/manage/server'
 require 'rightscaler/manage/status'
 
-
-# At some point in the past, ActiveResource couldn't manage to do a head
-# request right; this fixes it.
+# ActiveResource can't manage to do a head request right, this fixes it
 # https://rails.lighthouseapp.com/projects/8994/tickets/1223-activeresource-head-request-sends-headers-with-a-nil-key#ticket-1223-6
-# and https://rails.lighthouseapp.com/projects/8994/tickets/3020-activeresourceexists-does-not-work-at-all
-#
-# ActiveResource 3.0.0 fixes this problem (it may have been fixed earlier) and
-# also has a build_request_headers with arity of 3, so the fix below isn't
-# needed and doesn't work.
-
-require 'active_resource/version'
-
-unless ActiveResource::VERSION::MAJOR >= 3
-  module ActiveResource
-    class Connection
-      HTTP_FORMAT_HEADER_NAMES[:head] = 'Content-Type'
-      
-      def head(path, headers = {})
-        request(:head, path, build_request_headers(headers, :head))
-      end
+module ActiveResource
+  class Connection
+    HTTP_FORMAT_HEADER_NAMES[:head] = 'Content-Type'
+    
+    def head(path, headers = {})
+      request(:head, path, build_request_headers(headers, :head))
     end
   end
 end
